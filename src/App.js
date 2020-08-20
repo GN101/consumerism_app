7d41d3bfe6e00fe5d6721525630f410128cb2057
dimitrists4 <dim_ts@hotmail.gr>
2020-08-20 22:00:32 +0300

    i changed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          I changed the code
         </p>
         <a
           className="App-link"
