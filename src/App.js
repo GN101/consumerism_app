ae938f2b41b5f7d767427d825c0da07ea895211e
Nick Gkoulioumis <nickgoulioumis@protonmail.com>
2020-08-23 15:39:30 +0300

    changed repo name to new one

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          I changed the code
         </p>
         <a
           className="App-link"
