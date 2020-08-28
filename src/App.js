85183e8c7f2fd5ad6e9c651060cfe0b861fa012e
dimitrists4 <dim_ts@hotmail.gr>
2020-08-28 16:08:15 +0300

    Fixed linter warnings and list styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <main style={{marginTop:'100px'}}>
+      <main style={{ marginTop: '100px' }}>
         <p>
           Content
         </p>
