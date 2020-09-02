ef00af3c69aefbee2aab3b647b7dfc3cb96e18ca
Nick Gkoulioumis <nickgoulioumis@protonmail.com>
2020-09-02 08:16:24 +0300

    minor css fix

--- src/components/UserInputColumn/InputField/InputField.js
+++ src/components/UserInputColumn/InputField/InputField.js
@@ -2,12 +2,21 @@ import React from 'react';
 import styles from './InputField.module.css';
 
 const InputField = (props) => {
-  const { label, placeholder, category } = props;
+  const {
+    label, placeholder, category, changed, clicked 
+  } = props;
 
   return (
     <div className={styles.Form}>
       <label className={styles.Label} htmlFor={category}>{label}
-        <input className={styles.Input} type="text" id={category} placeholder={placeholder} />
+        <input
+          className={styles.Input}
+          type="text"
+          id={category}
+          placeholder={placeholder}
+          onChange={changed}
+          onClick={clicked}
+        />
       </label>
     </div>
   );
