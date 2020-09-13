162799e5c1546885ebb0c9237089d303a9e31011
Nick Gkoulioumis <nickgoulioumis@protonmail.com>
2020-09-13 23:16:54 +0300

    [#10] added type & ranged validation for user input

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import styles from './InputField.module.css';
+import WarningSuspicious from './WarningSuspicious/WarningSuspicious';
 
 const InputField = (props) => {
   const {
-    label, placeholder, category, classname, changed, clicked
+    label, placeholder, category, classname, valid, valRequired, isSuspicious, touched, changed, clicked
   } = props;
+  const inputClasses = [classname || styles.Input];
+  if (!valid && valRequired && touched) {
+    inputClasses.push(styles.Invalid);
+  }
+  // At least I tried :) TODO: let's discuss about this. Let me know if you have any ideas on this.
+  // const warningMsg = () => {
+  //   if (isSuspicious) {
+  //     console.log('test123');
+  //     setTimeout(() => (
+  //       <WarningSuspicious />
+  //     ), 2000);
+  //   }
+  // };
 
   return (
     <div className={styles.Form}>
       <label className={styles.Label} htmlFor={category}>{label}
+        {/* {warningMsg()} */}
+        {isSuspicious && <WarningSuspicious />}
         <input
-          className={classname || styles.Input}
+          className={inputClasses.join(' ')}
           type="text"
           id={label}
           name={label}
