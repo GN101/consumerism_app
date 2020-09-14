31a8d9e843a93ca9ccdbb0cbb2d05fb4388f9819
Nick Gkoulioumis <nickgoulioumis@protonmail.com>
2020-09-14 22:22:48 +0300

    added the delay in the validation warning panel

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './InputField.module.css';
 import WarningSuspicious from './WarningSuspicious/WarningSuspicious';
 
@@ -7,10 +7,21 @@ const InputField = (props) => {
     label, placeholder, category, classname, valid, valRequired, isSuspicious, touched, changed, clicked
   } = props;
   const inputClasses = [classname || styles.Input];
+  const [timerOn, setTimerOn] = useState(false);
+
   if (!valid && valRequired && touched) {
     inputClasses.push(styles.Invalid);
   }
   // At least I tried :) TODO: let's discuss about this. Let me know if you have any ideas on this.
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (isSuspicious) {
+        setTimerOn(true);
+      }
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [isSuspicious]);
   // const warningMsg = () => {
   //   if (isSuspicious) {
   //     console.log('test123');
@@ -24,7 +35,7 @@ const InputField = (props) => {
     <div className={styles.Form}>
       <label className={styles.Label} htmlFor={category}>{label}
         {/* {warningMsg()} */}
-        {isSuspicious && <WarningSuspicious />}
+        {isSuspicious && timerOn && <WarningSuspicious />}
         <input
           className={inputClasses.join(' ')}
           type="text"
