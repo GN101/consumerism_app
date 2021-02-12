ea33a4ba544758acd9bb9e7a8a655996ca9a88b1
miskas <xasimachos@gmail.com>
2021-02-12 18:28:59 +0200

    ADD[]#16  mocked input data, UsersAverageInput column

--- src/components/UsersAverageInputColumn/UsersAverageInputColumn.js
+++ src/components/UsersAverageInputColumn/UsersAverageInputColumn.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-// eslint-disable-next-line max-len
-// // import styles from './WorldDataColumn.module.css';
-// import WorldField from './WorldField';
-// import mockedUserInputState from '../../mocks/mockedUserInputColumnState';
+import React from 'react';
+import dataInputs from '../../mocks/dataInputs';
 
-class UsersAverageInputColumn extends Component {
-  state = {
-    totalCost: 2,
-  };
+const UsersAverageInputColumn = () => {
+  // const key = 'clothes';
+  // const average =
+  //   dataInputs.reduce((a, b) => (dataInputs[0][key] += b[key])) /
+  //   dataInputs.length;
+  // const result = Object.keys(dataInputs[0]).forEach((key) => average(key));
+  // console.log(result);
+  // console.log(dataInputs[1]['clothes']);
 
-  render() {
-    const count = this.state.totalCost;
-    return <div> exo {count} arxidia </div>;
-  }
-}
+  const average2 = dataInputs.reduce((basket, fruit) => {
+    for (const [key, value] of Object.entries(fruit)) {
+      if (!basket[key]) {
+        basket[key] = 0;
+      }
+      basket[key] += value;
+    }
+
+    return basket;
+  }, {});
+  console.log(average2);
+
+  return <div> {2} </div>;
+
+  // let filteredData = data.filter(({ gender }) => gender == 'female'),
+  // avg = filteredData.reduce((r, c) => r + c.age, 0) / filteredData.length;
+};
 
 export default UsersAverageInputColumn;
