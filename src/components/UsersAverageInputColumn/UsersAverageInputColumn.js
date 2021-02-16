ba7f0724cd80d749b625f3ad60a6e169b037c8f8
miskas <xasimachos@gmail.com>
2021-02-16 18:57:27 +0200

    ADD[16] added firebase data and fixes

--- src/components/UsersAverageInputColumn/UsersAverageInputColumn.js
+++ src/components/UsersAverageInputColumn/UsersAverageInputColumn.js
@@ -1,42 +1,67 @@
-import React from 'react';
-import dataInputs from '../../mocks/dataInputs';
+import React, { useEffect, useState } from 'react';
+import axios from '../../axios-orders';
 
 const UsersAverageInputColumn = () => {
-  const average = dataInputs.reduce((a, b) => {
-    for (const [key, value] of Object.entries(b)) {
-      if (!a[key]) {
-        a[key] = 0;
+  const [usersData, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/userData.json');
+        setData(response.data);
+      } catch (e) {
+        console.log(`Failure getting user input form - Error: ${e}`);
       }
-      a[key] += value / dataInputs.length;
-    }
-    return a;
+    };
+    fetchData();
   }, []);
 
-  const keys = Object.keys(average);
-  const values = Object.values(average);
-  const totalCost = values.reduce((a, b) => a + b);
+  const usersDataArr = Object.values(usersData);
 
-  return (
-    <div>
-      <h3>User Data averages!</h3>
-      <table>
-        <tbody>
-          {keys.map((key, index) => (
+  if (usersDataArr.length > 0) {
+    const usersInputs = [];
+    for (let i = 0; i < usersDataArr.length; i++) {
+      usersInputs.push(usersDataArr[i].categories);
+    }
+
+    const average = usersInputs.reduce((a, b) => {
+      for (const [key, value] of Object.entries(b)) {
+        if (!a[key]) {
+          a[key] = 0;
+        }
+        a[key] += value / usersInputs.length;
+      }
+      return a;
+    }, []);
+
+    const inputCategories = Object.keys(average);
+    const aveCost = Object.values(average);
+    const totalAveCost = aveCost.reduce((a, b) => a + b);
+
+    return (
+      <div>
+        <h3>User Data averages!</h3>
+        <table>
+          <tbody>
+            {inputCategories.map((category, index) => (
+              <tr>
+                <td>{category}</td>
+                <td>{aveCost[index].toFixed()}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
             <tr>
-              <td>{key}</td>
-              <td>{values[index].toFixed(1)}</td>
+              <td>Total Cost</td>
+              <td>{totalAveCost.toFixed()}</td>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Total Cost</td>
-            <td>{totalCost.toFixed(1)}</td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-  );
+          </tfoot>
+        </table>
+      </div>
+    );
+  } else {
+    return <div>loading</div>; //this gives a warning but I need it cause Async fetchData response [] and breaks code. does pretyfiy "malakizete"?
+  }
 };
 
 export default UsersAverageInputColumn;
