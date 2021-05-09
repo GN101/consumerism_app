9cf21aca4db01f5f222dc061492bc9472e7eeeff
miskas <xasimachos@gmail.com>
2021-05-09 13:14:12 +0300

    ADD[#5] UserPanel html , css ready

--- src/components/UserPanel/UserPanel.js
+++ src/components/UserPanel/UserPanel.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserPanel.module.css';
 
 const UserPanel = () => {
-  return <p className={styles.Container}>UserPanel</p>;
+  const [timeframe, setTimeframe] = useState('per Month');
+
+  return (
+    <div className={styles.Container}>
+      <h2 className={styles.Header}>Wellcome our fellow Consumer</h2>
+      <p className={styles.Text}>it's time to take care of your Expenses!</p>
+      <form>
+        <div className={styles.TimePeriod}>
+          <label className={styles.Label} htmlFor="input1">
+            How much do you wana save?
+          </label>
+          <select defaultValue={timeframe}>
+            <option value="per Week">per Week </option>
+            <option value="per Month">per Month</option>
+            <option value="per Year">per Year</option>
+          </select>
+          <input
+            className={styles.Input}
+            id="input1"
+            onChange={(event) => {
+              event.target.value = event.target.value.replace(/[\D]/, '');
+            }}
+          />
+          <label className={styles.Label} htmlFor="input2">
+            What's your income?
+          </label>
+          <select defaultValue={timeframe}>
+            <option value="per Week">per Week </option>
+            <option value="per Month">per Month</option>
+            <option value="per Year">per Year</option>
+          </select>
+          <input
+            className={styles.Input}
+            id="input2"
+            onChange={(event) => {
+              event.target.value = event.target.value.replace(/[\D]/, '');
+            }}
+          />
+        </div>
+        <button className={styles.Button}> Lets go </button>
+      </form>
+    </div>
+  );
 };
 
 export default UserPanel;
